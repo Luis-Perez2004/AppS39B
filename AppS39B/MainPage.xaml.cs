f2cf93bccc646e869380aef54dfa6a82f922ccd9
wizar <wizar@DESKTOP-K731FUB>
2021-10-22 21:00:21 -0500

    Agregar archivos de proyecto.

---
+++ AppS39B/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppS39B
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            //Alamacenar los datos en varibles
+            string usuario = txtusuario.Text;
+            string contraseña = txtcontraseña.Text;
+
+            //Navegando la ventana dos
+            await Navigation.PushAsync(new ViewDos(usuario,contraseña));
+        }
+    }
+}
---
+++ AppS39B/ViewDos.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppS39B
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewDos : ContentPage
+    {
+        public ViewDos(string Usuario, string Contraseña)
+        {
+            InitializeComponent();
+            //Envio a los label
+            lblUsuario.Text = Usuario;
+            lblContraseña.Text = Contraseña;
+            //Envio a las cajas de texto
+            txtUsuario.Text = Usuario;
+            txtContraseña.Text = Contraseña;
+        }
+    }
+}
